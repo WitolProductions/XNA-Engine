25c2f9733e4a0d65ce501e989bd9384899eb32fd
JustinWitol <JustinWitol@WitolProductions.com>
2012-08-10 18:40:51 -0400

    Content Creator Update
    Content Creator got an update and is now fully functional. We will use
    it to generate an XNB file for us that holds information on all content
    files in our project.

--- General/ContentGenerator/Files.cs
+++ General/ContentGenerator/Files.cs
@@ -14,8 +14,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml;
-using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+using Microsoft.Xna.Framework.Graphics;
+using Toolset.GraphicsDevice;
+using ServiceContainer = System.ComponentModel.Design.ServiceContainer;
 
 namespace ContentGenerator
 {
@@ -27,11 +34,41 @@ namespace ContentGenerator
         /// Your Content Folder
         /// </summary>
         const string Content = "Content";
-        
+
+        /// <summary>
+        /// Dummy form for creating a graphics device
+        /// </summary>
+        static readonly Form Form = new Form(); 
+
+        /// <summary>
+        /// Our Graphics Device Service, which holds all the data needed about our screen
+        /// </summary>
+        static GraphicsDeviceService _gds = null;
+
+        /// <summary>
+        /// Our Service Container
+        /// </summary>
+        static Toolset.GraphicsDevice.ServiceContainer _services = null;
+
+        /// <summary>
+        /// Our Content Manager
+        /// </summary>
+        static ContentManager _content;
+
         /// <summary>
         /// The File List
         /// </summary>
-        static List<string> _filesList = null;
+        static Dictionary<string, string> _filesList = null;
+
+        /// <summary>
+        /// Percent of files that has loaded in
+        /// </summary>
+        static double _percent = 0;
+
+        /// <summary>
+        /// Amount to jump at each point of change in percent
+        /// </summary>
+        static decimal _jump = 0;
 
         #endregion
 
@@ -42,7 +79,11 @@ namespace ContentGenerator
         /// </summary>
         static Files()
         {
-            _filesList = new List<string>();
+            _filesList = new Dictionary<string, string>();
+            _gds = GraphicsDeviceService.AddRef(Form.Handle, Form.ClientSize.Width, Form.ClientSize.Height);
+            _services = new Toolset.GraphicsDevice.ServiceContainer();
+            _services.AddService<IGraphicsDeviceService>(_gds);
+            _content = new ContentManager(_services, Content);
         }
 
         #endregion
@@ -57,14 +98,15 @@ namespace ContentGenerator
             if (Directory.Exists(Content))
             {
                 Console.WriteLine(Content + " folder found");
-
-                //Load all files inside Content
+                Console.Write("\rLoading In Files: {0}%", (int)_percent);
+                
+                //Load all files inside the folders inside Content, Calculate the jump to make, and test each file
                 LoadFiles(Content + "\\");
-                //Load all files inside the folders inside Content
-                foreach (var f in Directory.GetDirectories(Content))
-                    LoadFiles(f);
-
-                Serialize();
+                CalculateJump();
+                TestFiles();
+                
+                //Write out our file
+                Write(_filesList, null, Content + ".xnb", TargetPlatform.Windows);
             }
             else
             {
@@ -80,24 +122,106 @@ namespace ContentGenerator
         /// <param name="name"></param>
         static void LoadFiles(string name)
         {
-            //Only load files that end in .xnb and remove our Content folders name
-            foreach (var file in from f in Directory.GetFiles(name)
+
+            //Only load files that end in .xnb and remove our Content folders name);
+            foreach (var file in from f in Directory.GetFiles(name, "*", SearchOption.AllDirectories)
                                  where new FileInfo(f).Extension == ".xnb"
                                  select f.Replace(Content + "\\", "")
                                  into file select file.Replace(".xnb", ""))
-                _filesList.Add(file);
+            {
+                _filesList.Add(file, "Unknown");
+            }
+        }
+
+        /// <summary>
+        /// Test each file
+        /// </summary>
+        static void TestFiles()
+        {
+            var tempList = new Dictionary<string, string>();
+            foreach(var k in _filesList.Keys)
+            {
+                //Load in our file for testing and determine what type it is
+                var testFile = _content.Load<object>(k);
+                tempList.Add(k, testFile.GetType().Name);
+                
+                //Unload content because there is a 9
+                _content.Unload();
+
+                //Update our Percent and write it out
+                _percent += (double)_jump;
+                Console.Write("\rLoading In Files: {0}%", (int)_percent);
+            }
+
+            _filesList = tempList;
+        }
+
+        /// <summary>
+        /// Simply devide 100 into the total number of files being scanned in
+        /// </summary>
+        static void CalculateJump()
+        {
+            _jump = 100m/_filesList.Count;
         }
 
         #endregion
 
         #region XML Methods
 
-        static void Serialize()
+        /// <summary>
+        /// Write our XNB file using the data we have gathered in the previous methods
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="writer"></param>
+        /// <param name="fileName"></param>
+        /// <param name="targetPlatform"></param>
+        static void Write(Object obj, ContentTypeWriter writer, string fileName, TargetPlatform targetPlatform)
         {
-            //Serialize our File list
-            using (var writer = XmlWriter.Create(Content + ".xml", new XmlWriterSettings { Indent = true }))
-                IntermediateSerializer.Serialize(writer, _filesList, null);
-        }
+            //Use reflection to get the otherwise internal parameters and methods of the typewriter 
+            var constructors = typeof(ContentCompiler).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            var addWriter = typeof(ContentCompiler).GetMethod("AddTypeWriter", BindingFlags.NonPublic | BindingFlags.Instance);
+            var compileContent = typeof(ContentCompiler).GetMethod("Compile", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var compiler = constructors[0].Invoke(null) as ContentCompiler;
+
+            //Initialize our writter if it isn't
+            if (writer != null)
+                addWriter.Invoke(compiler, new object[] { writer });
+            
+            //Split the .xnb from the filename 
+            var path = fileName.Replace(".xnb", ""); 
+
+            //Generate our File
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                compileContent.Invoke(compiler,
+                    new [] 
+                    { 
+                         stream, 
+                         obj, 
+                         targetPlatform,
+                         GraphicsProfile.Reach,
+                         true,
+                         path, 
+                         path, 
+                    });
+            }
+
+            //Test our file
+            Console.WriteLine("");
+            Console.WriteLine("Testing file...");
+            
+            //Create our ContentManager
+            var services = new GameServiceContainer();
+            var content = new ContentManager(services, "");
+            //Load our File in
+            var test = content.Load<Dictionary<string, string>>(path);
+            //Write our files contents
+            foreach(var s in test)
+                Console.WriteLine(s);
+            //Wait for user responce
+            Console.ReadKey();
+        } 
 
         #endregion
     }
--- Graphics/Graphics/Texture/Content.cs
+++ Graphics/Graphics/Texture/Content.cs
@@ -48,13 +48,8 @@ namespace Graphics.Texture
             //Initialize our Textures class
             Textures.Initialize();
             
-            //Initialize our Root Directory
-            InitializeFolder(Content.RootDirectory);
-
-            //Now initialize all folders in our Root Directory
-            foreach (var d in Directory.GetDirectories(Content.RootDirectory))
-                InitializeFolder(d);
-
+            //Initialize our Content
+            InitializeContent("Content");
         }  
         
 
@@ -83,39 +78,32 @@ namespace Graphics.Texture
         /// <summary>
         /// Initialize all XNB files that are SpriteSheet files inside the directory passed
         /// </summary>
-        /// <param name="folderName"></param>
-        static void InitializeFolder(string folderName)
+        /// <param name="fileName"></param>
+        static void InitializeContent(string fileName)
         {
-            if (!Directory.Exists(folderName)) return; //Return if Directory passed does not exist
+            if (!File.Exists(fileName)) return; //Return if Directory passed does not exist
 
-            foreach(var f in Directory.GetFiles(folderName))
-            {
-                var fileInfo = new FileInfo(f);
 
-                if (!fileInfo.Exists) continue; //No idea why this should happen but incase it does...
-                if (fileInfo.Extension != ".xnb") continue; //Found odd file we don't need
-                SpriteSheet spriteSheet = null;
+            //SpriteSheet ss = null;
+            
+            //foreach(var content in ss.SpriteNames)
+            //{
+            //    SpriteSheet spriteSheet = null;
 
-                var filePath = folderName.Replace(Content.RootDirectory + "\\", ""); //Remove "Content\\"
-                filePath = filePath.Replace(Content.RootDirectory, ""); //Remove only "Content"
-                if (filePath != "")
-                    filePath += "\\";
-                filePath += Path.GetFileNameWithoutExtension(fileInfo.FullName); //Add the files name to create a real path
-
-                try
-                {
-                    spriteSheet = Content.Load<SpriteSheet>(filePath);
-                }
-                catch
-                { } //Unless there is another way to detect what kind of XNB this file belongs to then we need to ingore it when it errros
+            //    try
+            //    {
+            //        spriteSheet = Content.Load<SpriteSheet>(content.Key);
+            //    }
+            //    catch
+            //    { } //Unless there is another way to detect what kind of XNB this file belongs to then we need to ingore it when it errros
                         
-                //Ignore files that don't load into our variable
-                if (spriteSheet == null) continue;
-                spriteSheet.Path = filePath;
-                spriteSheet.Loaded = true;
-                spriteSheet.UnloadTimer = Constants.UnloadTimer;
-                Textures.Add(spriteSheet);
-            }
+            //    //Ignore files that don't load into our variable
+            //    if (spriteSheet == null) continue;
+            //    spriteSheet.Path = content.Key;
+            //    spriteSheet.Loaded = true;
+            //    spriteSheet.UnloadTimer = Constants.UnloadTimer;
+            //    Textures.Add(spriteSheet);
+            //}
         }
 
         #endregion
